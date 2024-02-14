2fdd6c8fc55271de3b0fce3532807092447442b8
karim368 <karimever251@gmail.com>
2024-02-15 00:07:16 +0530

    first commit

--- app/models.py
+++ app/models.py
@@ -2,10 +2,15 @@ from django.db import models
 
 # Create your models here.
 
+from django.urls import reverse
+
 class School(models.Model):
     sname = models.CharField(max_length = 100,unique = True)
     sprincipal = models.CharField(max_length = 100)
     slocation = models.CharField(max_length = 100)
+
+    def get_absolute_url(self):
+        return reverse('SchoolDetail',kwargs={'pk':self.pk})
 
     def __str__(self):
         return self.sname
--- app/views.py
+++ app/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 
 from app.models import *
-from django.views.generic import ListView,DetailView
+from django.views.generic import ListView,DetailView,CreateView,UpdateView
 
 class SchoolList(ListView):
     model = School
@@ -12,3 +12,11 @@ class SchoolList(ListView):
 class SchoolDetail(DetailView):
     model = School
     context_object_name = 'schoolobject'
+
+class SchoolCreate(CreateView):
+    model = School
+    fields = '__all__'
+
+class SchoolUpdate(UpdateView):
+    model = School
+    fields = '__all__'
