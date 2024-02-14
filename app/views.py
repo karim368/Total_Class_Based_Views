0cab7fd56da05670495e9b4cd7255d11f4313ac8
karim368 <karimever251@gmail.com>
2024-02-14 23:28:38 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -3,9 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 
 from app.models import *
-from django.views.generic import ListView
+from django.views.generic import ListView,DetailView
 
 class SchoolList(ListView):
     model = School
-    #fields = '__all__'
     context_object_name = 'schools'
+
+class SchoolDetail(DetailView):
+    model = School
+    context_object_name = 'schoolobject'
